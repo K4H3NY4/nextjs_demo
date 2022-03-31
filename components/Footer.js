06402731ef02262a6011f932b8594c65d72213a9
John Kahenya <johnkahenya@Johns-MacBook-Pro.local>
2022-04-01 00:05:41 +0300

    Components Added

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+
+        <nav>
+         <div>
+             <h1>Footer goes here</h1>
+         </div>
+        </nav>
+
+    );
+}
+
+export default Footer;
---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+
+        <nav>
+         <div>
+             <h1>List</h1>
+         </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
