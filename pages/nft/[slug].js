8555dcc27e93ce8bb9a84bcbd867dabe499f1000
John Kahenya <johnkahenya@Johns-MacBook-Pro.local>
2022-04-04 01:42:50 +0300

    nft pages done

--- pages/nft/[slug].js
+++ pages/nft/[slug].js
@@ -1,6 +1,8 @@
 import { createClient } from "contentful"
 import Image from "next/image"
 import styles from '../../styles/People.module.css'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Link from "next/link"
 
 const client = createClient ({
     space: "ucidywn4lkw6",
@@ -44,23 +46,57 @@ export default function NftDetails ( {nft} ){
     console.log(nft)
     return(
         <div>
-           <h1>{nft.fields.title}</h1>
-           <div>
+           
+           <div className="nft-details">
+               <div>
            <Image
                src={'https:' + nft.fields.thumbnail.fields.file.url}
                width={600 }
                height={600}
                />
+              
+          
                </div>
-               <div className={styles.single}>
+               <div>
             
-            <p className={styles.single} >{nft.fields.title}</p>
-            <p className={styles.single} >{nft.fields.desc}</p>
+            <h1>{nft.fields.title}</h1>
+            <p>{documentToReactComponents(nft.fields.desc)}</p>
+        <br/>
+            <Link href={nft.fields.storeLink}>
+            <a  className="store-link-btn"> STORE LINK</a>
+            </Link>
+           
   
-          
-          
+            
 
         </div>
+       
+        </div>
+
+        <style jsx>{`
+        .nft-details{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+        }
+
+        .store-link-btn{
+            background-color:#222;
+            padding: 10px;
+            color:#fff;
+            border-radius: 5px;
+            margin-top:60px;
+        }
+
+        .store-link-btn:hover{
+            background-color: #f01b29;
+            padding: 10px;
+            color:#fff;
+            border-radius: 5px;
+            margin-top:60px;
+        }
+        
+        `}</style>
         </div>
     )
 }
